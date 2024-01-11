7e11e6fc41ec17efc68a8863920c31d390c03936
re.t <apeach5@kakao.com>
2024-01-11 16:58:54 +0900

    [feat] simple Node.js example

--- 03. Node.js/webserver.js
+++ 03. Node.js/webserver.js
@@ -2,11 +2,32 @@ const http = require('http');
 const fs = require('fs').promises;
 
 const server = http.createServer(async (req, res) => {
-    // 헤더
-    res.writeHead(200, {'Content-Type': 'text/html; character=utf-8'});
-    // 바디
-    const data = await fs.readFile('./index.html');
-    res.end(data);
+    try {
+        console.log(req.method, req.url);
+        if (req.method === 'GET') {
+            let data = ''
+            if (req.url === '/') {
+                data = await fs.readFile('./index.html');
+            } else if (req.url === '/about') {
+                data = await fs.readFile('./about.html');
+            } else if (req.url === '/products') {
+                data = await fs.readFile('./products.html');
+            } else {
+                data = 'Page Not Found'
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(data);
+        } else {
+            console.log('[-] Currently Unavailable')
+            res.writeHead(404);
+            res.end('Cannot handle these type of method')
+        }
+    } catch (err) {
+        console.error(err);
+        res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end('Internal error occurred.');
+    }
 });
 
 server.on('request', function() {
