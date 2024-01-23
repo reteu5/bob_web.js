231ecc559e1bcc62a2e3cc0cfe29fcbedef7bedc
re.t <apeach5@kakao.com>
2024-01-23 16:37:45 +0900

    ♻️ refactor: improve in  printing cookie

--- 05. Session & Auth/cookie.js
+++ 05. Session & Auth/cookie.js
@@ -7,6 +7,7 @@ const port = 3080;
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
+    // maxAge는 ms 단위
     res.cookie('mycookie', 'test', { maxAge: 60000 });
     res.cookie('username', 'ret', { maxAge: 90000 });
     res.cookie('cart', ['사과우유', '딸기우유', '초코우유', '고수우유'], { maxAge: 120000 });
@@ -14,10 +15,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/user', (req, res) => {
-    const readCookie = req.cookies.mycookie;
+    const {username, cart, mycookie} = req.cookies.mycookie;
     console.log('req.cookie: ', req.cookies);
 
-    res.send(`Cookie you've brought: ${readCookie}`);
+    res.send(`${username} have brought: ${cart}`);
 });
 
 app.listen(port, () => {
